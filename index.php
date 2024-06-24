12812835de235af15495c2f7a899f4ae2c1d0410
MarcoDomingosSyone <marco.domingos@syone.com>
2024-06-24 21:12:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,3 @@ echo getenv("cidade");
 echo ""
 echo getenv("teamLeader");
 echo ""
-echo ""
-echo ""
- $url = "https://careers.recruiteecdn.com/image/upload/q_auto,f_auto,w_400,c_limit/production/images/BN5l/2dDjxgF-CXUi.png";
- $image = file_get_contents("$url");
- echo $image;
