c787799d5dbf625aa483b9c15a2df51080397ce7
MarcoDomingosSyone <marco.domingos@syone.com>
2024-06-24 22:02:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 </head>
 <body>
     <?php
-echo "Syone - 2024\n".
+echo "My name is\n".
 getenv("nome_completo") . "\n".
+echo "I am\n"
 getenv("idade") . "\n".
+echo "old.\n"
+echo "I am from\n"
 getenv("cidade") . "\n".
+echo "and my team leader is\n"
 getenv("teamLeader") . "\n";
 
 $image_url = "https://careers.recruiteecdn.com/image/upload/q_auto,f_auto,w_400,c_limit/production/images/BN5l/2dDjxgF-CXUi.png";
