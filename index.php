1ec7628491a7834cb798f8c9f789a2dc96a0022a
MarcoDomingosSyone <marco.domingos@syone.com>
2024-06-24 20:51:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello World!";
+echo "Syone - 2024";
+echo getenv("\tnome_completo\n");
+echo getenv("\tidade\n");
+echo getenv("\tcidade\n");
+echo getenv("\tteamLeader\n");
+
+ $url = "https://careers.recruiteecdn.com/image/upload/q_auto,f_auto,w_400,c_limit/production/images/BN5l/2dDjxgF-CXUi.png";
+     $image = file_get_contents("\t$url\n");
+     echo $image;
