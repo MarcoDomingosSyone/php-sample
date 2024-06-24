d8013ac4ee9ec0f191e227b8d515031416904fe1
MarcoDomingosSyone <marco.domingos@syone.com>
2024-06-24 22:11:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ getenv("idade").
 "I am from ".
 getenv("cidade") .
 "and my team leader is ".
-getenv("teamLeader") . "\n".;
+getenv("teamLeader") . "\n";
 
 $image_url = "https://careers.recruiteecdn.com/image/upload/q_auto,f_auto,w_400,c_limit/production/images/BN5l/2dDjxgF-CXUi.png";
 echo "<img src='" . $image_url . "' alt='Description of Image'>";
