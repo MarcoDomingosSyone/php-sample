e2a322d77215662d940543929a9d0afa95e7171a
MarcoDomingosSyone <marco.domingos@syone.com>
2024-06-24 21:25:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 echo "Syone - 2024";
-echo getenv("nome_completo"."\n".);
-echo getenv("idade"."\n".);
-echo getenv("cidade"."\n".);
-echo getenv("teamLeader"."\n".");
+echo getenv("nome_completo"."\n");
+echo getenv("idade"."\n");
+echo getenv("cidade"."\n");
+echo getenv("teamLeader"."\n");
 
 
